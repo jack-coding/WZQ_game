ed24341eceb03958ff48ba930f2f586785d683f4
jack-coding <2515906731@qq.com>
2020-07-26 19:14:04 +0800

    2.0

--- gamemodel.cpp
+++ gamemodel.cpp
@@ -10,13 +10,34 @@ void GameModel::startGame(GameType type)
     gameType=type;
     //初始化棋盘
     gameMapVec.clear();
-    for(int i=0;j<BOARD_GRAD_SIZE;i++){
+    for(int i=0;j<BOARD_GRAD_SIZE;i++)
+    {
         std::vector<int> lineBoard;
-        for(int j=0;j<BOARD_GRAD_SIZE;++j){
+        for(int j=0;j<BOARD_GRAD_SIZE;++j)
+        {
             lineBoard.push_back(0);
-            gameMapVec.push_back(lineBoard);
+        }
+        gameMapVec.push_back(lineBoard);
+    }
+
+    //如果是AI模式,需要初始化评分数组
+    if(gameType==AI)
+    {
+        scoreMapVec.clear();
+        for(int i=0;i<BOARD_GRAD_SIZE;++i)
+        {
+            std::vector<int> lineScores;
+            for(int j=0;j<BOARD_GRAD_SIZE;++j)
+            {
+                lineScores.push_back(0);
+            }
+            scoreMapVec.push_back(lineScores);
         }
     }
+
+    //轮到黑方下棋为true,白方下棋为false
+    playerFlag=true;
+
 }
 
 void GameModel::calculateScore()
--- mainwindow.h
+++ mainwindow.h
@@ -19,13 +19,13 @@ public:
 
 private:
     Ui::MainWindow *ui;
-
     GameModel *game;//游戏指针
     GameType game_type;//存储游戏类型
-
+    int clickPosRow,clickPosCol;
+    bool selectPos=false;//是否移动到合适的位置,已选中某个交叉点
     void initGame();
     void initAIGame();
-
+    void mouseMoveEvent(QMouseEvent *event);
     //绘制
     void paintEvent(QPaintEvent *event);
 };
